1368f0a5bd3099bfdc3aa96ae58a9464eb1b5f91
Tim Clark <t.i.j.clark@gmail.com>
2015-10-03 15:16:06 +0100

    Initial checkin

---
+++ life/life.h
@@ -0,0 +1,19 @@
+//
+//  life.h
+//  life
+//
+//  Created by Tim Clark on 03/10/2015.
+//  Copyright © 2015 Tim Clark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for life.
+FOUNDATION_EXPORT double lifeVersionNumber;
+
+//! Project version string for life.
+FOUNDATION_EXPORT const unsigned char lifeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <life/PublicHeader.h>
+
+
