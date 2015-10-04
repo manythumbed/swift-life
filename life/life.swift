2c0ded783144142066d8ef7d8e5b550836632572
Tim Clark <t.i.j.clark@gmail.com>
2015-10-04 20:36:20 +0100

    Minor additions

--- life/life.swift
+++ life/life.swift
@@ -3,3 +3,20 @@ import Foundation
 func stuff() -> Bool {
 	return true
 }
+
+struct Member:Hashable {
+	let x: Int
+	let y: Int
+	
+	var hashValue: Int {
+		return 31 &* x &+ y
+	}
+}
+
+func ==(lhs: Member, rhs:Member) -> Bool	{
+	return lhs.x == rhs.x && lhs.y == lhs.y
+}
+
+struct Universe {
+	var members = Set<Member>()
+}
